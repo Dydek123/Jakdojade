7dec554eff5ecb37f6d8d91885ca81162c406e1a
Dydek123 <mazurkiewicz707@gmail.com>
2020-03-13 16:33:14 +0100

    Początkowa-Końcowa

--- Jakdojade/venv/Lib/123.py
+++ Jakdojade/venv/Lib/123.py
@@ -13,9 +13,34 @@ koniec=("Krowodrza Górka", )
 c.execute("SELECT StopID FROM Points WHERE StopName=? order by StopID",koniec)
 print(c.fetchall())
 
-# c.execute("SELECT PointName FROM RRoutes WHERE StopID=73")
-# print(c.fetchall())
+
+print("StreetID poczatek")
+c.execute("SELECT StreetID FROM RRoutes WHERE StopID=73 order by StreetID")
+print(c.fetchall())
+
+print("StreetID koniec")
+c.execute("SELECT StreetID FROM RRoutes WHERE StopID=81 order by StreetID")
+print(c.fetchall())
 
 print("Wypisuje linie pomiędzy przystankiem początkowym a końcowym przystankiem linii")
-c.execute("SELECT LineName FROM StopDepartures WHERE StopID=73 and LastStopName=?",koniec)
+trasa = c.execute("SELECT LineName FROM StopDepartures WHERE StopID=73 and LastStopName=? order by LineName",koniec)
+for row in trasa:
+        print(row[0])
+
+
+# print("Przystanek poczatkowy")
+# start=(input(),)
+# print("Przystanek koncowy")
+# koniec=(input(),)
+#
+# print("Wypisuje linie i ilosc przystankow pomiędzy przystankiem początkowym a końcowym przystankiem linii")
+# c.execute("SELECT LineName,PointCount FROM Variants WHERE FirstStopName=(?) and LastStopName=(?)",(start,koniec))
+
+print("Wypisuje linie i ilosc przystankow pomiędzy przystankiem początkowym a końcowym przystankiem linii")
+c.execute("SELECT LineName,PointCount FROM Variants WHERE FirstStopName='Czyżyny Dworzec' and LastStopName='Mydlniki'")
 print(c.fetchall())
+for row in c.fetchall():
+    print(row)
+
+c.close()
+conn.close()
