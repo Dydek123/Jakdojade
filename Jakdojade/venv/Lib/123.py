7c67125bc90b0539d5fd9e2b7b64bcf7c4ff542a
Dydek123 <mazurkiewicz707@gmail.com>
2020-03-14 17:49:44 +0100

    Start/End PointID

--- Jakdojade/venv/Lib/123.py
+++ Jakdojade/venv/Lib/123.py
@@ -2,23 +2,27 @@ import sqlite3
 conn = sqlite3.connect("rozklady.sqlite3")
 c = conn.cursor()
 
-# print("Przystanek poczatkowy")
-# # start=(input(), )
-# start=("Politechnika", )
-# c.execute("SELECT StopID FROM Points WHERE StopName=? order by StopID",start)
-# print(c.fetchall())
-# print("Przystanek koncowy")
+print("\nPrzystanek poczatkowy")
+# start=(input(), )
+start=("Politechnika", )
+przystanek=c.execute("SELECT StopID FROM Points WHERE StopName=? order by StopID",start)
+for rows in przystanek:
+    startID=rows[0]
+print("StopID ",start[0],"= ",startID)
+
+print("\nPrzystanek koncowy")
 # koniec=(input(), )
-koniec=("Dworzec Główny Zachód", )
-# c.execute("SELECT StopID FROM Points WHERE StopName=? order by StopID",koniec)
-# print(c.fetchall())
-#
-#
-# print("StreetID poczatek")
+koniec=("Przybyszewskiego", )
+przystanek=c.execute("SELECT StopID FROM Points WHERE StopName=? order by StopID",koniec)
+for rows in przystanek:
+    koniecID=rows[0]
+print("StopID",koniec[0],"= ",koniecID)
+
+# print("\nStreetID poczatek")
 # c.execute("SELECT StreetID FROM RRoutes WHERE StopID=73 order by StreetID")
 # print(c.fetchall())
 #
-# print("StreetID koniec")
+# print("\nStreetID koniec")
 # c.execute("SELECT StreetID FROM RRoutes WHERE StopID=81 order by StreetID")
 # print(c.fetchall())
 
@@ -26,7 +30,7 @@ koniec=("Dworzec Główny Zachód", )
 x=[0,0,0,0,0,0,0,0,0,0,0,0,0]
 y=[]
 i=0
-print("Wypisuje linie pomiędzy przystankiem początkowym a końcowym przystankiem linii")
+print("\nWypisuje linie pomiędzy przystankiem Politechnika a końcowym Dworzec Główny Zachód")
 trasa = c.execute("SELECT LineName FROM StopDepartures WHERE StopID=73 and LastStopName=? order by LineName",koniec)
 for row in trasa:
 
@@ -39,19 +43,56 @@ print("y = ", y)
 
 
 
-# # print("Przystanek poczatkowy")
-# # start=(input(),)
-# # print("Przystanek koncowy")
-# # koniec=(input(),)
-# #
-# # print("Wypisuje linie i ilosc przystankow pomiędzy przystankiem początkowym a końcowym przystankiem linii")
-# # c.execute("SELECT LineName,PointCount FROM Variants WHERE FirstStopName=(?) and LastStopName=(?)",(start,koniec))
+# print("Przystanek poczatkowy")
+# start=(input(),)
+# print("Przystanek koncowy")
+# koniec=(input(),)
 #
 # print("Wypisuje linie i ilosc przystankow pomiędzy przystankiem początkowym a końcowym przystankiem linii")
-# c.execute("SELECT LineName,PointCount FROM Variants WHERE FirstStopName='Czyżyny Dworzec' and LastStopName='Mydlniki'")
-# print(c.fetchall())
-# for row in c.fetchall():
-#     print(row)
+# c.execute("SELECT LineName,PointCount FROM Variants WHERE FirstStopName=? and LastStopName=?",{start,koniec})
+# c.execute("SELECT LineName,PointCount FROM Variants WHERE FirstStopName=:start and LastStopName=:koniec",{"start": start, "koniec": koniec})
+
+print("\nWypisuje linie i ilosc przystankow pomiędzy przystankiem Czyżyny Dworzec a końcowym przystankiem Mydlniki")
+c.execute("SELECT LineName,PointCount FROM Variants WHERE FirstStopName='Czyżyny Dworzec' and LastStopName='Mydlniki'")
+print(c.fetchall())
+for row in c.fetchall():
+    print(row)
+
+
+
+
+
+print("\n\n\n\nPrzystanek poczatkowy")
+# start=(input(), )
+start=("Politechnika", )
+przystanek=c.execute("SELECT StopID FROM Points WHERE StopName=? order by StopID",start)
+for rows in przystanek:
+    cos=rows[0]
+startID=(cos,)
+print("StopID ",start[0],"= ",startID[0])
+
+print("\nPrzystanek koncowy")
+# koniec=(input(), )
+koniec=("Przybyszewskiego", )
+przystanek=c.execute("SELECT StopID FROM Points WHERE StopName=? order by StopID",koniec)
+for rows in przystanek:
+    cos=rows[0]
+koniecID=(cos,)
+print("StopID",koniec[0],"= ",koniecID[0])
+
+StartPointID = []
+print("\n StartPointID:")
+c.execute("SELECT ID From Points WHERE StopId=?",startID)
+for rows in przystanek:
+    StartPointID.append(rows[0])
+print(StartPointID)
+
+EndPointID = []
+print("\n EndPointID:")
+c.execute("SELECT ID From Points WHERE StopId=?",koniecID)
+for rows in przystanek:
+    EndPointID.append(rows[0])
+print(EndPointID)
 
 c.close()
 conn.close()
