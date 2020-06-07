40de8be67660dd226623978eb87696b9dd65a293
Dydek123 <mazurkiewicz707@gmail.com>
2020-06-07 18:07:14 +0200

    Dodane testy

--- test_szukaj.py
+++ test_szukaj.py
@@ -48,23 +48,46 @@ class WyszukiwainieTest(unittest.TestCase):
         self.assertRaises(Exception, self.wyszukiwarka.zamien_id_na_nr_linii, 25)
 
     def test_zamien_elementy_int_na_str(self):
+        """Tests if function convert integer to string."""
         self.assertEqual(self.wyszukiwarka.zamien_elementy_int_na_str([12,25]), ['12','25'])
         self.assertEqual(self.wyszukiwarka.zamien_elementy_int_na_str(['12','25']), ['12','25'])
         self.assertEqual(self.wyszukiwarka.zamien_elementy_int_na_str(['12',25]), ['12','25'])
 
     def test_ktory_przystanek_linii(self):
+        """Tests ktory_przystanek_linii function."""
         self.assertRaises(Exception, self.wyszukiwarka.ktory_przystanek_linii, 113, 1085738)
         self.assertRaises(Exception, self.wyszukiwarka.ktory_przystanek_linii, [113], [1085738])
 
     def test_ile_przystankow(self):
+        """Tests ile_przystankow function."""
         self.assertEqual(self.wyszukiwarka.ile_przystankow([2,5],[3,8]), [1,3])
         self.assertRaises(Exception, self.wyszukiwarka.ile_przystankow, 2, 5)
 
     def test_rzutuj_na_int(self):
+        """Tests if function convert string to integer."""
         self.assertEqual(self.wyszukiwarka.rzutuj_na_int(['2', '5']), [2, 5])
         self.assertRaises(Exception, self.wyszukiwarka.rzutuj_na_int, 2)
         self.assertRaises(Exception, self.wyszukiwarka.rzutuj_na_int, ['2','str'])
 
+    def test_szukaj_wszystkie_drogi(self):
+        """Tests szukaj_wszystkie drogi function."""
+        self.assertRaises(Exception, self.wyszukiwarka.rzutuj_na_int, 2098,12076)
+        self.assertRaises(Exception, self.wyszukiwarka.rzutuj_na_int, ['2', 'str'],['12350','12078'])
+
+    def test_wybierz_unikalne(self):
+        """Selects only unique routes."""
+        self.assertEqual(self.wyszukiwarka.wybierz_unikalne([[['12','15'],['12','15']]]), [['15', '12']])
+        self.assertRaises(Exception, self.wyszukiwarka.wybierz_unikalne, [['12','15'],['12','15']])
+
+    def test_wybierz_najkrotszy(self):
+        """Selects the shortest route."""
+        self.assertEqual(self.wyszukiwarka.wybierz_najkrotszy([[12],[12,15]]),[12])
+
+    def test_szukaj_polaczen(self):
+        """Tests search function."""
+        self.assertEqual(self.wyszukiwarka.szukaj_polaczen('Plac Inwalidów', 'AGH'),['292'])
+        self.assertRaises(Exception, self.wyszukiwarka.wybierz_unikalne, 'Plac Inwalidów', 'AGHH')
+        self.assertRaises(Exception, self.wyszukiwarka.wybierz_unikalne, 'Plac Inwalidów', '12')
 
 
 if __name__ == '__main__':
